1fc483cbd727adf0bbca8046e34a911e377a12f1
mimilettd <mimilettd@gmail.com>
2017-08-01 09:22:27 -0600

    build pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,22 @@
 class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = {}
+  end
 
+  def stock_check(item)
+    unless stock[item] == nil
+      return stock[item]
+    else
+      return 0
+    end
+  end
+
+  def restock(item, quantity)
+    if stock.include?(item)
+      stock[item] = stock[item] + quantity
+    else
+      stock.merge!(item => quantity)
+    end
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,35 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 
+  def test_it_starts_off_with_an_empty_stock
+    pantry = Pantry.new
+    expected = {}
+    assert_equal expected, pantry.stock
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock_again
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
 end
